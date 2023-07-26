c4ff078399925a180c642f8a2ba1ed11d88960ae
Parkseungje <tmdwprpdla@naver.com>
2023-07-27 01:33:59 +0900

    헤더 생성, 메인이미지 생성

--- src/App.js
+++ src/App.js
@@ -7,22 +7,30 @@ import OrderHistory from './header/OrderHistory';
 import Cart from './header/Cart';
 import Login from './header/Login';
 import Signup from './header/Signup';
+import MainImage from './header/MainImage';
 import './App.css'; // App.css에서 스타일을 정의합니다.
 
 const App = () => {
   return (
     <Router>
       <div className="navbar">
-        <Link to="/">홈</Link>
-        <Link to="/mypage">마이페이지</Link>
-        <Link to="/order-history">주문내역</Link>
-        <Link to="/cart">장바구니</Link>
-        <Link to="/login">로그인</Link>
-        <Link to="/signup">회원가입</Link>
+        <div className="left-menu">
+          <Link to="/">홈</Link>
+          <Link to="/mypage">마이페이지</Link>
+          <Link to="/order-history">주문내역</Link>
+          <Link to="/cart">장바구니</Link>
+        </div>
+        <div className="right-menu">
+          <Link to="/login">로그인</Link>
+          <Link to="/signup">회원가입</Link>
+        </div>
+      </div>
+      <div>
+        <Link to="/"><MainImage /></Link> 
       </div>
       <hr />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route exact path="/" element={<Home/>} />
         <Route path="/mypage" element={<MyPage />} />
         <Route path="/order-history" element={<OrderHistory />} />
         <Route path="/cart" element={<Cart />} />
---
+++ src/header/MainImage.js
@@ -0,0 +1,12 @@
+// 이미지를 public 폴더에 저장한 경우
+import React from 'react';
+
+const MainImage = () => {
+  return (
+    <div>
+      <img src="/main_image/carevillege_thumbnail.jpeg" alt="Example" />
+    </div>
+  );
+};
+
+export default MainImage;
