0b4b48d3ec24679aee6e3caab075be56d7994c2e
Parkseungje <tmdwprpdla@naver.com>
2023-08-02 02:21:06 +0900

    메인 스와이퍼 추가

--- src/header/Home.js
+++ src/header/Home.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import SwiperMain from '../body/SwiperMain';
 
 const Home = () => {
   return (
-    <div>
-      <h2>Home Page</h2>
-      <p>This is the about page content.</p>
-    </div>
+    <>
+      <SwiperMain></SwiperMain>
+      <div>
+        <h2>Home Page</h2>
+        <p>This is the about page content.</p>
+      </div>
+    </>
   );
 };
 
